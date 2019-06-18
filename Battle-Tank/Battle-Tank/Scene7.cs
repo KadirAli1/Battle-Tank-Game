137917ecbc2871d5599579823887736443f6fa99
Fisnik Limani <fisnik.limani1@gmail.com>
2019-06-18 18:26:29 +0200

    Scene7 added

---
+++ Battle-Tank/Battle-Tank/Scene7.cs
@@ -0,0 +1,130 @@
+﻿using Battle_Tank.Properties;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battle_Tank
+{
+    public class Scene7 : Scene
+    {
+        public Scene7(int Width, int Height)
+            : base(4, 6, 95, 95, Width, Height, Color.FromArgb(232, 240, 240))
+        {
+
+            // TOP
+            for (int i = 0; i < 6; ++i)
+            {
+                Walls.Add(new Wall(new Rectangle((i * ColumnWidth) + Scene.LEFT, Scene.TOP, ColumnWidth, 8), Resources.Scene7_InnerWallHorizontal));
+            }
+
+            // BOTTOM
+            for (int i = 0; i < 6; ++i)
+            {
+                Walls.Add(new Wall(new Rectangle(i * ColumnWidth + Scene.LEFT, Rows * RowHeight + Scene.TOP, ColumnWidth, 8), Resources.Scene7_InnerWallHorizontal));
+            }
+
+            // LEFT
+            for (int i = 0; i < 4; ++i)
+            {
+                Walls.Add(new Wall(new Rectangle(Scene.LEFT, i * RowHeight + Scene.TOP, 8, RowHeight), Resources.Scene7_InnerWallVertical));
+            }
+
+            // RIGHT
+            for (int i = 0; i < 4; ++i)
+            {
+                Walls.Add(new Wall(new Rectangle(Scene.LEFT + Columns * ColumnWidth, i * RowHeight + Scene.TOP, 8, RowHeight), Resources.Scene7_InnerWallVertical));
+            }
+
+
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            // OUTER TOP
+            for (int i = 0; i < 6; ++i)
+            {
+                Walls.Add(new Wall(new Rectangle((i * ColumnWidth) + Scene.LEFT, Scene.TOP - 16, ColumnWidth, 16), Resources.Scene7_OuterWallTop));
+            }
+
+            // OUTER BOTTOM
+            for (int i = 0; i < 6; ++i)
+            {
+                Walls.Add(new Wall(new Rectangle(i * ColumnWidth + Scene.LEFT, Rows * RowHeight + Scene.TOP + 8, ColumnWidth, 16), Resources.Scene7_OuterWallBottom));
+            }
+
+            // OUTER LEFT
+            for (int i = 0; i < 4; ++i)
+            {
+                Walls.Add(new Wall(new Rectangle(Scene.LEFT - 16, i * RowHeight + Scene.TOP, 16, RowHeight), Resources.Scene7_OuterWallLeft));
+            }
+
+            // OUTER RIGHT
+            for (int i = 0; i < 4; ++i)
+            {
+                Walls.Add(new Wall(new Rectangle(Scene.LEFT + Columns * ColumnWidth + 8, i * RowHeight + Scene.TOP, 16, RowHeight), Resources.Scene7_OuterWallRight));
+            }
+
+
+
+            ////////////////////////////////////////////////////////////////////////////////
+            // ADDING LINES INSIDE
+            // FIRST COLUMN
+            // HORIZONTAL
+            // VERTICAL
+            Walls.Add(new Wall(new Rectangle(Scene.LEFT + 1 * ColumnWidth, 1 * RowHeight + Scene.TOP, 8, RowHeight), Resources.Scene7_InnerWallVertical));
+
+            // SECOND COLUMN
+            // HORIZONTAL
+            Walls.Add(new Wall(new Rectangle(Scene.LEFT + 1 * ColumnWidth, 2 * RowHeight + Scene.TOP, ColumnWidth, 8), Resources.Scene7_InnerWallHorizontal));
+            Walls.Add(new Wall(new Rectangle(Scene.LEFT + 1 * ColumnWidth, 3 * RowHeight + Scene.TOP, ColumnWidth, 8), Resources.Scene7_InnerWallHorizontal));
+            // VERTICAL
+            Walls.Add(new Wall(new Rectangle(Scene.LEFT + 2 * ColumnWidth, 0 * RowHeight + Scene.TOP, 8, RowHeight), Resources.Scene7_InnerWallVertical));
+
+            // THIRD COLUMN
+            // HORIZONTAL
+            Walls.Add(new Wall(new Rectangle(Scene.LEFT + 2 * ColumnWidth, 3 * RowHeight + Scene.TOP, ColumnWidth, 8), Resources.Scene7_InnerWallHorizontal));
+            // VERTICAL
+            Walls.Add(new Wall(new Rectangle(Scene.LEFT + 3 * ColumnWidth, 3 * RowHeight + Scene.TOP, 8, RowHeight), Resources.Scene7_InnerWallVertical));
+
+
+            // FOURTH COLUMN
+            // HORIZONTAL
+            Walls.Add(new Wall(new Rectangle(Scene.LEFT + 3 * ColumnWidth, 2 * RowHeight + Scene.TOP, ColumnWidth, 8), Resources.Scene7_InnerWallHorizontal));
+            // VERTICAL
+            Walls.Add(new Wall(new Rectangle(Scene.LEFT + 4 * ColumnWidth, 0 * RowHeight + Scene.TOP, 8, RowHeight), Resources.Scene7_InnerWallVertical));
+            Walls.Add(new Wall(new Rectangle(Scene.LEFT + 4 * ColumnWidth, 3 * RowHeight + Scene.TOP, 8, RowHeight), Resources.Scene7_InnerWallVertical));
+
+
+            // FIFTH COLUMN
+            // HORIZONTAL
+
+            // VERTICAL
+            Walls.Add(new Wall(new Rectangle(Scene.LEFT + 5 * ColumnWidth, 3 * RowHeight + Scene.TOP, 8, RowHeight), Resources.Scene7_InnerWallVertical));
+
+
+            // SIXTH COLUMN
+            // HORIZONTAL
+            Walls.Add(new Wall(new Rectangle(Scene.LEFT + 5 * ColumnWidth, 1 * RowHeight + Scene.TOP, ColumnWidth, 8), Resources.Scene7_InnerWallHorizontal));
+            Walls.Add(new Wall(new Rectangle(Scene.LEFT + 5 * ColumnWidth, 2 * RowHeight + Scene.TOP, ColumnWidth, 8), Resources.Scene7_InnerWallHorizontal));
+            // VERTICAL
+
+
+
+
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            // ANGLES OF SCENE DECOR
+            // TOP-LEFT
+            Walls.Add(new Wall(new Rectangle(Scene.LEFT - 16, Scene.TOP - 16, 19, 19), Resources.Scene7_TopLeft));
+
+            // TOP-RIGHT
+            Walls.Add(new Wall(new Rectangle(Scene.LEFT + Columns * ColumnWidth + 2, Scene.TOP - 17, 19, 19), Resources.Scene7_TopRight));
+
+            // BOTTOM-LEFT
+            Walls.Add(new Wall(new Rectangle(Scene.LEFT - 16, Rows * RowHeight + Scene.TOP + 2, 19, 19), Resources.Scene7_BottomLeft));
+
+            // BOTTOM-RIGHT
+            Walls.Add(new Wall(new Rectangle(Scene.LEFT + Columns * ColumnWidth + 3, Rows * RowHeight + Scene.TOP + 3, 19, 19), Resources.Scene7_BottomRight));
+
+        }
+    }
+}
