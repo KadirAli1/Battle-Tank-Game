d1a65451fa5a646776f7a6652b79e6f5df3bf09f
Fisnik Limani <fisnik.limani1@gmail.com>
2019-06-19 19:19:10 +0200

    Scenes class updated to generate a random scene

--- Battle-Tank/Battle-Tank/Scenes.cs
+++ Battle-Tank/Battle-Tank/Scenes.cs
@@ -11,25 +11,62 @@ namespace Battle_Tank
     public class Scenes
     {
         public List<Scene> ScenesList { get; set; }
+        public Scene ActualScene { get; set; }
 
         public Scenes(int Width, int Height)
         {
-            ScenesList = new List<Scene>();
-            CreateScenes(Width, Height);
+            GenerateScene(Width, Height);
         }
 
-        private void CreateScenes(int Width, int Height)
+        public void GenerateScene(int Width, int Height)
         {
-            ScenesList.Add(new Scene6(Width, Height));
-        }
+            Random random = new Random();
 
-            
-        public void Draw(Pen pen, Graphics g)
-        {
-            foreach(Scene scene in ScenesList)
+            int r = 1;
+                //random.Next(5, 6);
+
+            switch (r)
             {
-                scene.Draw(pen, g);
+                case 1:
+                    ActualScene = new Scene1(Width, Height);
+                    break;
+                case 2:
+                    ActualScene = new Scene2(Width, Height);
+                    break;
+                case 3:
+                    ActualScene = new Scene3(Width, Height);
+                    break;
+                case 4:
+                    ActualScene = new Scene4(Width, Height);
+                    break;
+                case 5:
+                    ActualScene = new Scene5(Width, Height);
+                    break;
+                case 6:
+                    ActualScene = new Scene6(Width, Height);
+                    break;
+                case 7:
+                    ActualScene = new Scene7(Width, Height);
+                    break;
+                case 8:
+                    ActualScene = new Scene8(Width, Height);
+                    break;
+                case 9:
+                    ActualScene = new Scene9(Width, Height);
+                    break;
+                case 10:
+                    ActualScene = new Scene10(Width, Height);
+                    break;
+                default:
+                    ActualScene = new Scene1(Width, Height);
+                    break;
             }
+        }
+            
+        public void Draw(Pen pen, Graphics g, SceneForm sceneForm)
+        {
+            ActualScene.Draw(pen, g);
+            ActualScene.DrawOtherElements(sceneForm);
         }
     }
 }
