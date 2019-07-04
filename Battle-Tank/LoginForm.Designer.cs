c00a9b76ca6e094bea6411f4e5d88de8614a17e7
mustran <45851256+mustran@users.noreply.github.com>
2019-07-04 02:19:03 +0200

    Project Added

---
+++ Battle-Tank/LoginForm.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace Battle_Tank
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblBest = new System.Windows.Forms.Label();
+            this.lblPlayer1 = new System.Windows.Forms.Label();
+            this.lblPlayer2 = new System.Windows.Forms.Label();
+            this.lblLoginPlayer1 = new System.Windows.Forms.Label();
+            this.lblLoginPlayer2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tbUsername = new MetroFramework.Controls.MetroTextBox();
+            this.tbPassword = new MetroFramework.Controls.MetroTextBox();
+            this.btnPlayM = new MetroFramework.Controls.MetroButton();
+            this.lblSpace = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(144, 161);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 2;
+            this.btnLogin.Text = "Log in";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Location = new System.Drawing.Point(226, 161);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(75, 23);
+            this.btnRegister.TabIndex = 3;
+            this.btnRegister.Text = "Register";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 205);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Best User";
+            // 
+            // lblBest
+            // 
+            this.lblBest.AutoSize = true;
+            this.lblBest.Location = new System.Drawing.Point(89, 205);
+            this.lblBest.Name = "lblBest";
+            this.lblBest.Size = new System.Drawing.Size(35, 13);
+            this.lblBest.TabIndex = 8;
+            this.lblBest.Text = "points";
+            this.lblBest.Click += new System.EventHandler(this.lblBest_Click);
+            // 
+            // lblPlayer1
+            // 
+            this.lblPlayer1.AutoSize = true;
+            this.lblPlayer1.Location = new System.Drawing.Point(334, 79);
+            this.lblPlayer1.Name = "lblPlayer1";
+            this.lblPlayer1.Size = new System.Drawing.Size(48, 13);
+            this.lblPlayer1.TabIndex = 9;
+            this.lblPlayer1.Text = "Player 1:";
+            // 
+            // lblPlayer2
+            // 
+            this.lblPlayer2.AutoSize = true;
+            this.lblPlayer2.Location = new System.Drawing.Point(334, 111);
+            this.lblPlayer2.Name = "lblPlayer2";
+            this.lblPlayer2.Size = new System.Drawing.Size(48, 13);
+            this.lblPlayer2.TabIndex = 10;
+            this.lblPlayer2.Text = "Player 2:";
+            // 
+            // lblLoginPlayer1
+            // 
+            this.lblLoginPlayer1.AutoSize = true;
+            this.lblLoginPlayer1.Location = new System.Drawing.Point(399, 79);
+            this.lblLoginPlayer1.Name = "lblLoginPlayer1";
+            this.lblLoginPlayer1.Size = new System.Drawing.Size(0, 13);
+            this.lblLoginPlayer1.TabIndex = 11;
+            // 
+            // lblLoginPlayer2
+            // 
+            this.lblLoginPlayer2.AutoSize = true;
+            this.lblLoginPlayer2.Location = new System.Drawing.Point(399, 111);
+            this.lblLoginPlayer2.Name = "lblLoginPlayer2";
+            this.lblLoginPlayer2.Size = new System.Drawing.Size(0, 13);
+            this.lblLoginPlayer2.TabIndex = 12;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(16, 77);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(122, 98);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tbUsername
+            // 
+            // 
+            // 
+            // 
+            this.tbUsername.CustomButton.Image = null;
+            this.tbUsername.CustomButton.Location = new System.Drawing.Point(142, 1);
+            this.tbUsername.CustomButton.Name = "";
+            this.tbUsername.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.tbUsername.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.tbUsername.CustomButton.TabIndex = 1;
+            this.tbUsername.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.tbUsername.CustomButton.UseSelectable = true;
+            this.tbUsername.CustomButton.Visible = false;
+            this.tbUsername.DisplayIcon = true;
+            this.tbUsername.Icon = ((System.Drawing.Image)(resources.GetObject("tbUsername.Icon")));
+            this.tbUsername.Lines = new string[0];
+            this.tbUsername.Location = new System.Drawing.Point(146, 77);
+            this.tbUsername.MaxLength = 32767;
+            this.tbUsername.Name = "tbUsername";
+            this.tbUsername.PasswordChar = '\0';
+            this.tbUsername.PromptText = "Enter your username";
+            this.tbUsername.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.tbUsername.SelectedText = "";
+            this.tbUsername.SelectionLength = 0;
+            this.tbUsername.SelectionStart = 0;
+            this.tbUsername.ShortcutsEnabled = true;
+            this.tbUsername.Size = new System.Drawing.Size(164, 23);
+            this.tbUsername.TabIndex = 16;
+            this.tbUsername.UseSelectable = true;
+            this.tbUsername.WaterMark = "Enter your username";
+            this.tbUsername.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.tbUsername.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            this.tbUsername.Click += new System.EventHandler(this.tbUsernameM_Click);
+            // 
+            // tbPassword
+            // 
+            // 
+            // 
+            // 
+            this.tbPassword.CustomButton.Image = null;
+            this.tbPassword.CustomButton.Location = new System.Drawing.Point(142, 1);
+            this.tbPassword.CustomButton.Name = "";
+            this.tbPassword.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.tbPassword.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.tbPassword.CustomButton.TabIndex = 1;
+            this.tbPassword.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.tbPassword.CustomButton.UseSelectable = true;
+            this.tbPassword.CustomButton.Visible = false;
+            this.tbPassword.DisplayIcon = true;
+            this.tbPassword.Icon = ((System.Drawing.Image)(resources.GetObject("tbPassword.Icon")));
+            this.tbPassword.Lines = new string[0];
+            this.tbPassword.Location = new System.Drawing.Point(146, 106);
+            this.tbPassword.MaxLength = 32767;
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.PasswordChar = '*';
+            this.tbPassword.PromptText = "Enter your password";
+            this.tbPassword.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.tbPassword.SelectedText = "";
+            this.tbPassword.SelectionLength = 0;
+            this.tbPassword.SelectionStart = 0;
+            this.tbPassword.ShortcutsEnabled = true;
+            this.tbPassword.Size = new System.Drawing.Size(164, 23);
+            this.tbPassword.TabIndex = 17;
+            this.tbPassword.UseSelectable = true;
+            this.tbPassword.WaterMark = "Enter your password";
+            this.tbPassword.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.tbPassword.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // btnPlayM
+            // 
+            this.btnPlayM.BackColor = System.Drawing.Color.Red;
+            this.btnPlayM.Enabled = false;
+            this.btnPlayM.Location = new System.Drawing.Point(337, 161);
+            this.btnPlayM.Name = "btnPlayM";
+            this.btnPlayM.Size = new System.Drawing.Size(75, 23);
+            this.btnPlayM.TabIndex = 18;
+            this.btnPlayM.Text = "Play";
+            this.btnPlayM.UseSelectable = true;
+            this.btnPlayM.Click += new System.EventHandler(this.btnPlayM_Click);
+            // 
+            // lblSpace
+            // 
+            this.lblSpace.AutoSize = true;
+            this.lblSpace.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSpace.Location = new System.Drawing.Point(148, 205);
+            this.lblSpace.Name = "lblSpace";
+            this.lblSpace.Size = new System.Drawing.Size(0, 20);
+            this.lblSpace.TabIndex = 19;
+            // 
+            // LoginForm
+            // 
+            this.AcceptButton = this.btnLogin;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(580, 292);
+            this.Controls.Add(this.lblSpace);
+            this.Controls.Add(this.btnPlayM);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.tbUsername);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblLoginPlayer2);
+            this.Controls.Add(this.lblLoginPlayer1);
+            this.Controls.Add(this.lblPlayer2);
+            this.Controls.Add(this.lblPlayer1);
+            this.Controls.Add(this.lblBest);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.btnLogin);
+            this.Name = "LoginForm";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblBest;
+        private System.Windows.Forms.Label lblPlayer1;
+        private System.Windows.Forms.Label lblPlayer2;
+        private System.Windows.Forms.Label lblLoginPlayer1;
+        private System.Windows.Forms.Label lblLoginPlayer2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private MetroFramework.Controls.MetroTextBox tbUsername;
+        private MetroFramework.Controls.MetroTextBox tbPassword;
+        private MetroFramework.Controls.MetroButton btnPlayM;
+        private System.Windows.Forms.Label lblSpace;
+    }
+}
+
---
+++ Battle-Tank/LoginForm.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Battle_Tank
+{
+    
+    public partial class LoginForm : MetroFramework.Forms.MetroForm
+    {
+        static bool loggedFirst = false;
+        SqlConnection connection;
+        string connectionString;
+        public LoginForm()
+        {
+            InitializeComponent();
+            connectionString = ConfigurationManager.ConnectionStrings["Battle_Tank.Properties.Settings.UsersDatabaseConnectionString"].ConnectionString;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tbUsername.Focus();
+            string query = "SELECT MAX(Points) FROM Users";
+            using (connection = new SqlConnection(connectionString))
+            using (SqlCommand SqlQuery = new SqlCommand(query, connection))
+            {
+                connection.Open();
+                int result = (int)SqlQuery.ExecuteScalar();
+                lblBest.Text = result.ToString();
+            }
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            string query = "INSERT INTO Users VALUES(@UserName, @UserPassword, 0)";
+            string query1 = "SELECT COUNT(*) FROM [Users] WHERE ([UserName] = @user)";
+
+            using (connection = new SqlConnection(connectionString))
+            using (SqlCommand command1 = new SqlCommand(query1, connection))
+            {
+                connection.Open();
+                command1.Parameters.AddWithValue("@user", tbUsername.Text);
+
+                if ((int)command1.ExecuteScalar() > 0)
+                {
+                    MessageBox.Show("User " + tbUsername.Text + " exists. Try another username");
+                    tbUsername.Focus();
+                    return;
+                }
+            }
+
+            //int userExists = (int)checkUser.ExecuteScalar();
+
+            using (connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                connection.Open();
+
+                if (tbUsername.Text == "" || tbPassword.Text == "")
+                {
+                    MessageBox.Show("Username or password cannot be empty");
+                    tbUsername.Focus();
+                    return;
+                }
+                command.Parameters.AddWithValue("@UserName", tbUsername.Text);
+                command.Parameters.AddWithValue("@UserPassword", tbPassword.Text);
+                //   command.Parameters.AddWithValue("@Points", 0);
+
+                command.ExecuteNonQuery();
+            }
+
+            tbUsername.Text = "";
+            tbPassword.Text = "";
+            tbUsername.Focus();
+            MessageBox.Show("User Registered Succesfully");
+            //DialogResult = DialogResult.OK;
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            bool logInSuccesfully = false;
+            connection = new SqlConnection(connectionString);
+            connection.Open();
+            SqlCommand checkUser = new SqlCommand("SELECT COUNT(*) FROM[Users] WHERE ([UserName] = @userName) " +
+                "AND ([UserPassword] = @userPassword)", connection);
+            checkUser.Parameters.AddWithValue("@userName", tbUsername.Text);
+            checkUser.Parameters.AddWithValue("@userPassword", tbPassword.Text);
+            int userExists = (int)checkUser.ExecuteScalar();
+
+            if(userExists > 0)
+            {
+                MessageBox.Show("Log in succesfully");
+                if (!loggedFirst)
+                {
+                    lblLoginPlayer1.Text = tbUsername.Text;
+                    tbUsername.Text = "";
+                    tbPassword.Text = "";
+                    tbUsername.Focus();
+                    loggedFirst = true;
+                }
+       
+                else if (loggedFirst)
+                {
+                    lblLoginPlayer2.Text = tbUsername.Text;
+                    tbUsername.Text = "";
+                    tbPassword.Text = "";
+                    loggedFirst = false;
+                    btnPlayM.Enabled = true;
+                    btnPlayM.Focus();
+                    lblSpace.Text = "Press SPACE or ENTER to play the game!";
+                    AcceptButton = btnPlayM;
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Incorrect username or password");
+            }
+            connection.Close();
+        }
+
+        private void lblBest_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("PRESSED");
+        }
+
+        private void tbUsernameM_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPlayM_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("PLAY PRESSED");
+        }
+    }
+}
